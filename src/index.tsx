454a97cd38e00c3f8350555e1a175adb8883869a
Suad Emre Umar <suademre.umar@antwerpes.com>
2022-01-19 14:49:49 +0100

    Add register page and functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,20 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./store";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Uygulamamizin Store ni olusturuyoruz
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+//  Combine reducer medhodunu kullanarak uygulamamizin hangi state parcalarina ihtiyaci oldugunu belirticez
+
+import { combineReducers } from "redux";
+import { UserState } from "../types/user";
+import UserReducer from "./reducers/userReducer";
+
+// Backenden 3 ayri veri tipi var
+// 1- Users
+// 2- Records
+// 3- Categories
+
+interface AppState {
+  user: UserState;
+  //   categories: any;
+  //   records: any;
+}
+
+const rootReducer = combineReducers<AppState>({
+  user: UserReducer,
+  //   categories: () => {},
+  //   records: () => {},
+});
+
+export default rootReducer;
